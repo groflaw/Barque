af0fd3c0d694b11f91e1e0665c922b54488e395d
groflaw <romanphoon@gmail.com>
2024-12-11 13:04:59 +0300

    update payment detail

--- src/Components/Reservars/PaymentDetail.jsx
+++ src/Components/Reservars/PaymentDetail.jsx
@@ -8,42 +8,69 @@ import {
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector, useDispatch } from "react-redux";
+
+import { setLoading } from "../../Store/Global";
 
 const PaymentDetail = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const curbooking = useSelector((state) => state.Global.curbooking);
+  const [plan, setPlan] = useState({});
 
   const nextStep = () => {
     navigation.navigate("SelectPayment");
   };
+  useEffect(() => {
+    const fetchBoatTypes = async () => {
+      await dispatch(setLoading(true));
+      let result = curbooking.boatId.plans.find(
+        (item) => item._id === curbooking.planId
+      );
+      setPlan(result);
+      await dispatch(setLoading(false));
+    };
+
+    const unsubscribe = navigation.addListener("focus", async () => {
+      fetchBoatTypes();
+    });
+    return unsubscribe;
+  }, [navigation]);
   return (
     <View style={styles.container}>
       <View style={styles.card}>
         <Text style={styles.title}>Payment Details</Text>
         <View className="flex flex-row justify-between mt-3">
           <Text style={styles.item}>Plane Price</Text>
-          <Text style={styles.item}>$450</Text>
+          <Text style={styles.item}>${plan.price}</Text>
         </View>
         <View className="flex flex-row justify-between mt-3">
           <Text style={styles.item}>Service Fee</Text>
-          <Text style={styles.item}>$45</Text>
+          <Text style={styles.item}>${plan.price * 0.1}</Text>
         </View>
         <View style={styles.hr} />
         <View className="flex flex-row justify-between mt-3">
           <Text style={styles.total}>Total Amount</Text>
-          <Text style={styles.total}>$500</Text>
+          <Text style={styles.total}>
+            $
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(plan.price * 1.1)}
+          </Text>
         </View>
       </View>
-        <View style={styles.btn} className="mt-3">
-          <TouchableOpacity
-            onPress={() => {
-              nextStep();
-            }}
-          >
-            <Text style={styles.btntext} className="text-center text-white">
-              Proceed with Payment
-            </Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.btn} className="mt-3">
+        <TouchableOpacity
+          onPress={() => {
+            nextStep();
+          }}
+        >
+          <Text style={styles.btntext} className="text-center text-white">
+            Proceed with Payment
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
